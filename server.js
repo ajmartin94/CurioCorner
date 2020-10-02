43aea7dfa2fd927f057b73d5f88239d28fd1871f
Andrew Martin <martinandrew2@johndeere.com>
2020-10-02 15:11:19 -0500

    Testing heroku deployment

--- server.js
+++ server.js
@@ -9,5 +9,7 @@ app.get('/',(req,res) => {
     res.render('index.ejs')
 })
 
+console.log('I think this works')
+
 app.listen(process.env.PORT, () => console.log('Running on port '+process.env.PORT));
 
