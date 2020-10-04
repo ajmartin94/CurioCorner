262c410f394df2ccb0f6024ebc7769706d4bf1e1
Andrew Martin <martinandrew2@johndeere.com>
2020-10-04 14:11:20 -0500

    Added signup render route and fixed pathing for both signup and login render

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,9 +2,14 @@ const Auth = require('../models').Auth;
 const Users = require('../models').Users;
 
 const renderLogin = (req,res) => {
-    res.render('users/login.ejs')
+    res.render('login.ejs')
+}
+
+const renderSignUp = (req,res) => {
+    res.render('signUp.ejs')
 }
 
 module.exports = {
-    renderLogin
+    renderLogin,
+    renderSignUp
 }
--- routes/auth.js
+++ routes/auth.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const ctrl = require('../controllers');
 
 router.get('/login',ctrl.auth.renderLogin);
+router.get('/signup',ctrl.auth.renderSignUp)
 
 module.exports = router;
