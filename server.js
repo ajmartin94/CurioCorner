68e6fed82f6c8cb1be033be68acbcb870a69fffe
Andrew Martin <martinandrew2@johndeere.com>
2020-10-04 14:00:33 -0500

    Fixed method override error

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const routes = require('./routes');
+const methodOverride = require('method-override')
 
 app.use(methodOverride('_method'));
 app.use(express.urlencoded({extended: true}));
